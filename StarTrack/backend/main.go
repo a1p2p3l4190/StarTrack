2e178f4875b554dd7e406069ae70b4f249555e6b
Laura Liu <a1p2p3l4190@gmail.com>
2026-08-18 17:26:51 -0500

    fix CORS

--- StarTrack/backend/main.go
+++ StarTrack/backend/main.go
@@ -47,17 +47,11 @@ func main() {
 func setupRouter(cfg *Config) *gin.Engine {
 	router := gin.Default()
 	router.Use(cors.New(cors.Config{
-		// AllowOrigins:     []string{"*"},
-		AllowOrigins: []string{
-			"http://localhost:8081",
-			"http://localhost:8082",
-			"https://main.d4wh807cv4n11.amplifyapp.com",
-			"https://star-track-eta.vercel.app",
-		},
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
+		AllowCredentials: false,
 		MaxAge:           12 * time.Hour,
 	}))
 	router.Use(func(c *gin.Context) {
